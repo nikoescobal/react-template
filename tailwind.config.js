477f9a932b6f4eb30497bbf08be2510fe92e6c31
Niko Escobal <niko.escobal@gmail.com>
2021-10-26 22:36:22 +0800

    Update Tailwindconfig.js with correct purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './dist/**/*.html',
-  ],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        nunito: ['"Nunito"', 'sans-serif'],
-        italianno: ['"Italianno"', 'cursive'],
-        raleway: ['"Raleway"', 'sans-serif'],
+        nunito: ['"Nunito"', "sans-serif"],
+        italianno: ['"Italianno"', "cursive"],
+        raleway: ['"Raleway"', "sans-serif"],
       },
     },
   },
